278b2d09c4acc9c2156e97ce9f990af6bc9efeca
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-03 16:57:08 +0900

    :recycle: VPC追加

---
+++ modules/vpc/main.tf
@@ -0,0 +1,11 @@
+/************************************************************
+VPC
+************************************************************/
+resource "aws_vpc" "this" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "vpc"
+  }
+}
