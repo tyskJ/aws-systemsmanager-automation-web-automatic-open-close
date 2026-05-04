1cca7959205264d2e444057580ad85c3dc2a89ef
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-04 20:05:26 +0900

    :recycle: Change calender追加

---
+++ modules/systems_manager/main.tf
@@ -0,0 +1,32 @@
+/************************************************************
+Documents
+************************************************************/
+### Change Calender
+resource "aws_ssm_document" "change_calender" {
+  name            = "auto-open-close-calender"
+  document_type   = "ChangeCalendar"
+  document_format = "TEXT"
+  content         = <<-EOT
+    BEGIN:VCALENDAR
+    PRODID:-//AWS//Change Calendar 1.0//EN
+    VERSION:2.0
+    X-CALENDAR-TYPE:DEFAULT_CLOSED
+    X-WR-CALDESC:Calendar for managing automated opening and closing stations
+    X-CALENDAR-CMEVENTS:DISABLED
+    BEGIN:VEVENT
+    DTSTAMP:20260504T101206Z
+    UID:1566dd11-c8e4-4e1f-a55c-29410503c8a8
+    SEQUENCE:0
+    SUMMARY:open-event
+    DESCRIPTION:Operating hours
+    DTSTART;TZID=Asia/Tokyo:20260504T080000
+    DTEND;TZID=Asia/Tokyo:20260504T180000
+    RRULE:FREQ=WEEKLY;INTERVAL=1;BYDAY=MO,TU,WE,TH,FR
+    X-EVENT-TYPE:STANDARD
+    END:VEVENT
+    END:VCALENDAR
+  EOT
+  tags = {
+    Name = "auto-open-close-calender"
+  }
+}
